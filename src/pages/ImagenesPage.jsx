083d8ec5e7f832f1d31ed55e322871fc75d1f804
miguelfcma <107379032+miguelfcma@users.noreply.github.com>
2023-03-01 16:03:44 -0600

    agregue imagenes

--- src/pages/ImagenesPage.jsx
+++ src/pages/ImagenesPage.jsx
@@ -1,14 +1,79 @@
+import { useState } from "react";
+import { createImagenRequest } from "../API/imagenUsuario.api";
 
+function ImageForm() {
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [archivo, setArchivo] = useState(null);
+  const [error, setError] = useState("");
 
-function ImagenesPage() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!nombre || !archivo) {
+      setError("El nombre y el archivo son campos obligatorios");
+      return;
+    }
+
+    try {
+      const imagen = {
+        nombre,
+        descripcion,
+        archivo,
+      };
+
+      const response = await createImagenRequest(imagen);
+
+      console.log("Imagen creada:", response.data);
+
+      // Reiniciar los valores del formulario después de enviar la imagen.
+      setNombre("");
+      setDescripcion("");
+      setArchivo(null);
+      setError("");
+    } catch (error) {
+      console.error(error);
+      setError("Se ha producido un error al crear la imagen");
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const archivoSeleccionado = event.target.files[0];
+    setArchivo(archivoSeleccionado);
+  };
+
   return (
-    <div>
-        <h1>Imagenes</h1>
-
-        <input type="file" name="" id="" onChange={e => console.log(e.target.files[0])}/>
-
-    </div>
-  )
+    <form onSubmit={handleSubmit}>
+      {error && <div>{error}</div>}
+      <div>
+        <label htmlFor="nombre">Nombre:</label>
+        <input
+          id="nombre"
+          type="text"
+          value={nombre}
+          onChange={(event) => setNombre(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="descripcion">Descripción:</label>
+        <textarea
+          id="descripcion"
+          value={descripcion}
+          onChange={(event) => setDescripcion(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="archivo">Archivo:</label>
+        <input
+          id="archivo"
+          type="file"
+          accept="image/*"
+          onChange={handleFileChange}
+        />
+      </div>
+      <button type="submit">Crear imagen</button>
+    </form>
+  );
 }
 
-export default ImagenesPage
+export default ImageForm;
